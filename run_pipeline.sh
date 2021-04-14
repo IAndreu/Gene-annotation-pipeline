637b7d6b0d74679e9b443a6285d20e782359e509
Ignasi Andreu Godall <33428731+IAndreu@users.noreply.github.com>
2021-04-14 11:19:45 +0200

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -11,4 +11,4 @@ num_threads=4
 
 ############################
 
-python3 Scripts/run_analysis.py $proteome $genome_directory $gff $genome $run_bitacora $genome_name $num_threads
+python3 Scripts/run_analysis.py $path $proteome $genome_directory $gff $genome $run_bitacora $genome_name $num_threads
