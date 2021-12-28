5eaae23a24af76a727c93def4acb6c3cf72d194a
Ignasi Andreu <33428731+IAndreu@users.noreply.github.com>
2021-12-28 11:28:26 +0100

    Update run_pipeline_computerome.sh

--- run_pipeline_computerome.sh
+++ run_pipeline_computerome.sh
@@ -10,7 +10,7 @@
 ### Only send mail when job is aborted or terminates abnormally
 #PBS -m n
 ### Number of nodes
-#PBS -l nodes=1:ppn=4
+#PBS -l nodes=1:ppn=5
 ### Memory
 #PBS -l mem=40gb
 ### Requesting time - format is <days>:<hours>:<minutes>:<seconds> (here 12:00:00, 12 hours)
@@ -44,13 +44,16 @@ module load mafft/7.453
 
 ###### EDIT VARIABLES ######
 path="/path/to/Gene-annotation-pipeline"
+table="/path/to/gene_families.xlsx"
+gene_fam_db="/path/to/Gene_families_db"
 genome_name="Genome-name"
-genome_directory="${path}/Data/Genomes/Genome-name"
+output_directory="${path}/Data/Genomes/Genome-name"
 proteome="${genome_directory}/Proteome-in-fasta-file"
 gff="${genome_directory}/gff3-file"
 genome="${genome_directory}/Genome-in-fasta-file"
+interpro=${genome_directory}/predicted-domains-from-InterPro-in-TSV-format"
 run_bitacora="${path}/bitacora-master/runBITACORA_command_line.sh"
 num_threads=4
 ############################
 
-python3 Scripts/run_analysis.py $path $proteome $genome_directory $gff $genome $run_bitacora $genome_name $num_threads
+python3 Scripts/run_analysis.py $path $table $gene_fam_db $proteome $interpro $gff $genome $run_bitacora $genome_name $output_directory $num_threads
